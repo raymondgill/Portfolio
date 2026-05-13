5693a45db69070293600b44542b45f083b976d36
raymondgill <raymondgill2001@gmail.com>
2026-05-13 20:47:46 +0500

    ' change the commit message. Lines starting

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -80,9 +80,9 @@ const Contact = () => {
       {showAlert && <Alert type={alertType} text={alertMessage} />}
       <div className="flex flex-col items-center justify-center max-w-md p-5 mx-auto border border-white/10 rounded-2xl bg-primary">
         <div className="flex flex-col items-start w-full gap-5 mb-10">
-          <h2 className="text-heading">Let's Talk</h2>
+          <h2 className="text-heading">Let&apos;s Talk</h2>
           <p className="font-normal text-neutral-400">
-            Whether you're looking to build a new website, improve your existing
+            Whether you&apos;re looking to build a new website, improve your existing
             platform, or bring a unique project to life, We are here to help...
           </p>
         </div>
